c7e02851b8671a8e1e16801a104ad8fd2b1f201f
Hasan COMAK <hasan.comak@hasans-macbook-pro.flixbus.com>
2019-01-31 17:24:52 +0100

    Prepare Project

---
+++ verbum-cursus-api/src/main/java/net/entrofi/verbumcursus/verbumcursus/VerbumCursusApplication.java
@@ -0,0 +1,14 @@
+package net.entrofi.verbumcursus.verbumcursus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbumCursusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbumCursusApplication.class, args);
+	}
+
+}
+
