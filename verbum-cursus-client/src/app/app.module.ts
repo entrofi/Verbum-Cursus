0f0cdaacbea43a1cf2d14c86dea153774a01a53d
Hasan Comak <entrofi@gmail.com>
2019-03-06 16:01:42 +0100

    Add Ngrx Support and dockerize client
    Ngrx support is added to the client application and a landing page has
    been created.
    Client application is dockerised for kubernetes demonstration

--- verbum-cursus-client/src/app/app.module.ts
+++ verbum-cursus-client/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { metaReducers, reducers } from './store/reducers';
+
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
+import { environment } from '../environments/environment';
+import { MatIconModule, MatListModule, MatMenuModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatMenuModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({ stateKey: 'router', }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:
+    [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
